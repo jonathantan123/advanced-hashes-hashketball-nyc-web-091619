5bf533364eeebb47635ae120f3740575d8834905
Jonathan Tan <jonathantan@brandeis.edu>
2019-09-15 22:35:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -242,10 +242,10 @@ end
 def winning_team
   total_points = 0
   win_team = ''
-  game_hash.each do |team, place|
+  game_hash.each do |home_away, keys|
     team_points = 0
-    team_name = game_hash[team][:team_name]
-    team[:players].each do |player|
+    team_name = game_hash[home_away][:team_name]
+    keys[:players].each do |player|
       points = player[:points]
       team_points += points
     end
