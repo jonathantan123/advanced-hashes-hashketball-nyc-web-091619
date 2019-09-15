243d88ff77360fbca5bb03404e887c26ad57562a
Jonathan Tan <jonathantan@brandeis.edu>
2019-09-15 22:31:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -244,7 +244,7 @@ def winning_team
   win_team = ''
   game_hash.each do |place, team|
     team_points = 0
-    team_name = game_hash[home_away][:team_name]
+    team_name = game_hash[place][:team_name]
     keys[:players].each do |player|
       points = player[:points]
       team_points += points
